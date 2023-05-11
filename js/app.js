9385154a1e87ab122f62b0119e428182aa4e55a5
danielaser <daniela.serrano.dev@gmail.com>
2023-05-11 15:24:07 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const sectionPage = new fullpage('#fullpage', {
+    autoScrolling: true,
+    fitToSection: false,
+    fitToSectionDelay: 300,
+    easing: 'easeInOutCubic',
+    scrollingSpeed: 700,
+    css3: true,
+    easingcss3: 'ease-out',
+    loopBottom: false,
+    navigation: true,
+    menu: '#menu',
+    anchors: ['home', 'about', 'contact'],
+    navigationTooltips: ['Home', 'About', 'Contact'],
+    showActiveTooltip: false,
+    sectionsColor: ['#000', '#c2c2c2', '#000'],
+    verticalCentered: true,
+    controlArrows: true,
+    slidesNavigation: false,
+    afterLoad: function (origin, destination) {
+        if (destination.anchor == 'contact') {
+            document.querySelector('.footer').querySelector('h2').style.opacity = 1;
+        }
+    }
+});
